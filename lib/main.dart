40ab970e2340733ce957de56f69281897ceaff09
shahanah <shahanahbp@gmail.com>
2022-04-20 22:08:33 +0400

    Timer Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,122 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:woof/widget/button_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Woof());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
+class Woof extends StatefulWidget {
+  @override
+  _WoofState createState() => _WoofState();
+}
+
+class _WoofState extends State<Woof> {
+  static const maxSeconds = 30;
+  int seconds = maxSeconds;
+
+  Timer? timer;
+
+  void resetTimer() => setState(() => seconds = maxSeconds);
+
+  void startTimer(bool reset) {
+    if (reset) {
+      resetTimer();
+    }
+
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (seconds > 0) {
+        setState(() => seconds--);
+      } else {
+        stopTimer(false);
+      }
+    });
+  }
+
+  void stopTimer(bool reset) {
+    if (reset) {
+      resetTimer();
+    }
+    setState(() => timer?.cancel());
+  }
 
   @override
-  Widget build (BuildContext){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title : 'Timer',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Woof'),
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildTimer(),
+                const SizedBox(height: 80),
+                buildButtons(),
+              ],
+            ),
+          ),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+      );
+
+  Widget buildButtons() {
+    final isRunning = timer == null ? false : timer!.isActive;
+    final isCompleted = seconds == maxSeconds || seconds == 0;
+
+    return isRunning || !isCompleted
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonWidget(
+                text: isRunning ? 'Pause' : 'Resume',
+                onClicked: () {
+                  if (isRunning) {
+                    stopTimer(false);
+                  } else {
+                    startTimer(false);
+                  }
+                },
+              ),
+              const SizedBox(width: 12),
+              ButtonWidget(
+                text: 'Cancel',
+                onClicked: () {
+                  stopTimer(true);
+                },
+              ),
+            ],
+          )
+        : ButtonWidget(
+            text: 'Start',
+            color: Colors.black,
+            backgroundColor: Colors.white,
+            onClicked: () {
+              startTimer(true);
+            },
+          );
+  }
+
+  Widget buildTimer() => SizedBox(
+    width: 200,
+    height: 200,
+    child: Stack(
+      fit: StackFit.expand,
+      children: [
+        CircularProgressIndicator(
+          value: seconds / maxSeconds,
+          strokeWidth: 12,
+          valueColor: AlwaysStoppedAnimation(Colors.black),
+          backgroundColor: Colors.blueAccent,
         ),
-      )
+        Center(child: buildTime())
+      ],
+    ),
+  );
+
+  Widget buildTime() {
+    return Text(
+      '$seconds',
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.black, fontSize: 80),
     );
   }
 }
---
+++ lib/widget/button_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Color backgroundColor;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    this.color = Colors.white,
+    this.backgroundColor = Colors.black,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: backgroundColor,
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20, color: color),
+        ),
+        onPressed: onClicked,
+      );
+}
